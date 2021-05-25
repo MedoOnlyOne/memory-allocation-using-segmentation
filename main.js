7c5d03bc629939b6b14cec1fbbc2a06ae580e3f1
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2021-05-25 22:57:44 +0200

    ejs in use

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const ejs = require('ejs-electron');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -12,12 +13,13 @@ app.on('ready', ()=>{
     // create the main  window
     mainWindow = new BrowserWindow({});
 
-    // load the html
+    // load the ejs
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
+        pathname: path.join(__dirname, "index.ejs"),
         protocol: 'file:',
-        slashes: true       // file://dirname/index.html
+        slashes: true       // file://dirname/index.ejs
     }));
-
-
+    
+    // remove the menu when finished
+    // Menu.setApplicationMenu(null);
 });
