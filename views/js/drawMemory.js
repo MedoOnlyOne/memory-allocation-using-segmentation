8b5a7d78789914eacc75af536cf8b868456710d2
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2021-05-29 14:36:47 +0200

    Draw memory

--- views/js/drawMemory.js
+++ views/js/drawMemory.js
@@ -2,24 +2,47 @@ drawMemory = blocks => {
     const memoryElement = document.createElement('div');
     memoryElement.classList.add('memory-output');
     const blockElements = [];
-    let totalSize = 0;
-    for ( block of blocks ){
-        totalSize += block.size;
-    }
-    for ( block of blocks ){
+    // let totalSize = 0;
+    // for ( block of blocks ){
+    //     totalSize += block.size;
+    // }
+    let totalSize = parseInt(document.querySelector('#memory_size').value);
+    for ( let i = 0; i < blocks.length; i++ ){
+        const blockElement = document.createElement('div');
+        blockElement.classList.add('memory-block');
+        if (blocks[i].type === "process"){
+            blockElement.classList.add('process');
+        } else if (blocks[i].type === "hole"){
+            blockElement.classList.add('hole');
+        } else {
+            blockElement.classList.add('old-process');
+        }
         if (totalSize <= 500){
-            const blockElement = document.createElement('div');
-            blockElement.classList.add('memory-block');
-            blockElement.style = `height: ${block.size}px;`
-            blockElement.textContent = block.name;
-            blockElements.push(blockElement);
+            blockElement.style = `height: ${blocks[i].size}px;`
         }
         else{
-            const blockElement = document.createElement('div');
-            blockElement.classList.add('memory-block');
-            blockElement.style = `height: ${block.size*500/totalSize}px;`
-            blockElement.textContent = block.name;
-            blockElements.push(blockElement);
+            blockElement.style = `height: ${blocks[i].size*500/totalSize}px;`
+        }
+        const blockContent = document.createElement('h2');
+        blockContent.textContent = blocks[i].name;
+        blockContent.classList.add('block-content');
+        blockElement.appendChild(blockContent);
+        blockElements.push(blockElement);
+        if (i == 0){
+            const top = document.createElement('span');
+            top.textContent = 0;
+            top.classList.add('memory-size-top');
+            blockElement.appendChild(top);
+            const bottom = document.createElement('span');
+            bottom.textContent = `${blocks[0].size}`;
+            bottom.classList.add('memory-size-bottom');
+            blockElement.appendChild(bottom);
+        } else {
+            const bottom = document.createElement('span');
+            let bottom_ans = blocks[i].base + blocks[i].size;
+            bottom.textContent = `${bottom_ans}`;
+            bottom.classList.add('memory-size-bottom');
+            blockElement.appendChild(bottom);
         }
     }
     for ( blockElement of blockElements ){
