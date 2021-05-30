8f3f7ed8973f25bd092ef1b57966cff98c396958
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2021-05-30 19:30:25 +0200

    backButton, last edit

--- views/js/backButton.js
+++ views/js/backButton.js
@@ -7,14 +7,15 @@ const backButtonEventHandler = () => {
             position = 0;
         } 
         if (position === 0){
-            holes_cnt = 0;
-            holes = [];
-            document.querySelector('#hole_num').textContent = holes_cnt;
             const back = document.querySelector('#back-button');
             back.classList.add('back-button-disabled');
             back.classList.remove('back-button');
             for (let i=0; i<forms.length;i++)
                 forms[i].style='';
+
+            document.querySelector('#memory_size').value = '';
+            document.querySelector('#holes_number').value = '';
+            document.querySelector('#memory_submit').disabled = true;
         }    
     } else {
         forms[0].style='transform: translateX(-100vw);'   
@@ -27,26 +28,35 @@ const backButtonEventHandler = () => {
         for(let i = position + 1; i<forms.length; i++) 
             forms[i].style='';
         
-        if (position === 1){
+            if (position === 1){
+                holes_cnt = 0;
+                holes = [];
+                document.querySelector('#hole_num').textContent = holes_cnt;
+                document.querySelector('#hole_base').value = '';
+                document.querySelector('#hole_size').value = '';
+                document.querySelector('#holes_submit').disabled = true;
+            } else if (position === 2){
             processes_cnt = 0;
             document.querySelector('#process_cnt').textContent = processes_cnt;
-        } else if (position === 2){
+            document.querySelector('#processes_number').value = '';
+            document.querySelector('#processes_num_submit').disabled = true;
+        } else if (position === 3){
             segments_cnt = 0;
             processes_cnt = 0;
             processes = [];
             document.querySelector('#process_cnt').textContent = processes_cnt;
             document.querySelector('#segmenet_cnt').textContent = segments_cnt;
             document.querySelector('#segments_number').disabled = false;
-            document.querySelector('#segments_number_submit').disabled = false;
+            document.querySelector('#segments_number').value = '';
+            document.querySelector('#segments_number_submit').disabled = true;
             document.querySelector('.segments_info').style = "display: none";
-        } else if (position === 3){
-    
+            document.querySelector('#segment_name').value = '';
+            document.querySelector('#segment_size').value = '';
+            document.querySelector('#segment_submit').disabled = true;
         } else if (position === 4) {
             blocks = [];
-            let resultChildren = document.querySelector('.result');
-            while (resultChildren.hasChildNodes()) {  
-                resultChildren.removeChild(resultChildren.firstChild);
-              }
+            donnotFit = [];
+            document.querySelector('.result').innerHTML = '';
         }
     }
 
